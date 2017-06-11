20a769985821223dd0c5f2daf637b75b2d63dc61
Kristian Larsson <kristian@tutamail.com>
2017-06-11 16:34:09 -0700

    creates a png identicon image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,20 +3,50 @@ defmodule Identicon do
     Identicon creates a 5x5 image from a string.
   """
 
-  @doc """
-    Hashes the string to a list
-  ## Examples
-
-      iex> Identicon.main("banana")
-      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115,
-             1, 35, 239, 239, 124, 65]}
-
-  """
   def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map grid, fn({_num, index}) ->
+        x1 = rem(index, 5) * 50
+        y1 = div(index, 5) * 50
+        top_left = {x1, y1}
+        bottom_right = {x1 + 50, y1 + 50}
+        {top_left, bottom_right}
+      end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter grid, fn({num, _index}) ->
+        rem(num, 2) == 0
+      end
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
