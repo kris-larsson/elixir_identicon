e5b7aadf6e6a6023e90b5ee89920a1016b308651
Kristian Larsson <kristian@tutamail.com>
2017-06-11 15:25:35 -0700

    added struct with grid and color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,45 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    Identicon creates a 5x5 image from a string.
   """
 
   @doc """
-  Hello world.
-
+    Hashes the string to a list
   ## Examples
 
-      iex> Identicon.hello
-      :world
+      iex> Identicon.main("banana")
+      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115,
+             1, 35, 239, 239, 124, 65]}
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      Enum.chunk(hex, 3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
